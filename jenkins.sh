2b5024d57b93fcc4a18314ef9596639b499e47d9
Jason A. Hurst <hurst_jason@bah.com>
2019-06-04 09:47:49 -0400

    fixed install typo on yum statement

--- jenkins.sh
+++ jenkins.sh
@@ -2,6 +2,6 @@
 sudo yum -y install java-1.8.0-openjdk
 curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
 sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
-sudo yum install jenkins
+sudo yum install -y jenkins
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
