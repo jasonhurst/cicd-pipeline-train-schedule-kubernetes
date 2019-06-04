805e313a3f599d25740e82a3b43d06786c3b109c
Jason A. Hurst <hurst_jason@bah.com>
2019-06-04 10:19:38 -0400

    changed a url issue with gitlab

--- gitlab.sh
+++ gitlab.sh
@@ -9,5 +9,5 @@ sudo systemctl enable postfix
 sudo systemctl start postfix
 curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.rpm.sh | sudo bash
 sudo yum install -y gitlab-ce-11.10.4-ce.0.el7.x86_64
-sudo sed -i 's#external_url .*#external_url '"'"'http://192.168.100.10'"'"'#' /etc/gitlab/gitlab.rb
+sudo sed -i 's#external_url .*#external_url '"'"'http://192.168.100.11'"'"'#' /etc/gitlab/gitlab.rb
 sudo gitlab-ctl reconfigure
